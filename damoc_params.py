2908b6865639b3b630556eadf4058098043db9b2
mariaduvaz <maria.niculescu-duvaz.13@ucl.ac.uk>
2022-03-25 13:46:42 +0000

    tkinter GUI functional to same state as before when using matplotlib widgets. all widgets working now

--- damoc_params.py
+++ damoc_params.py
@@ -12,7 +12,6 @@ import tkinter.font as TkFont
 import os
 import damocleslib as model
 import numpy as np
-#import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
 from matplotlib.backends.backend_tkagg import (
@@ -24,9 +23,6 @@ import fileinput
 from FUNCTIONS import *
 from mpl_toolkits import mplot3d
 import numpy as np
-
-#from matplotlib.widgets import Slider,Button,CheckButtons,TextBox
-from matplotlib.artist import Artist
 from matplotlib.figure import Figure
 ###
 
@@ -202,6 +198,7 @@ class Plotting_window():
         self.obswav,self.obsflux= datafile_2_array(obsfile,isint=False,zipped=True)
         self.obswav,self.obsflux = trim_wav_flux(self.obswav,self.obsflux,trim_lims[0],trim_lims[1])
         self.obsflux = snip_spect(self.obswav,self.obsflux,*snip_regs)
+        self.obsflux = [i-0.4e-16 for i in obsflux]
         
         
         tk.Label(frame_b,text = 'Optical depth (\u03C4) ',font=self.buttonfont).pack(anchor='w',side=tk.LEFT)
@@ -233,7 +230,7 @@ class Plotting_window():
     
      def plot_model(self,frame):
          
-          modvel,modflux,modflux_e= datafile_2_array(self.outfile,isint=False,zipped=True)
+          modvel,modflux,modflux_e = datafile_2_array(self.outfile,isint=False,zipped=True)
           modflux= [i * np.amax(self.obsflux)/np.amax(modflux) for i in modflux]
           self.ax.plot(modvel,modflux) 
           self.figure_canvas.draw()
@@ -250,16 +247,28 @@ class Plotting_window():
             labelDir= tk.Label(frame, textvariable=labelText, height=3,font=self.buttonfont)
             labelDir.pack(fill='x',side=tk.LEFT,padx='20',pady='10')
          
-            scale_var = tk.StringVar()
-            scale_var.trace("w", self.scalebox_command(scale_var))
+            scale_var = tk.StringVar(value="1.0")
             
-            scale_var_entry = tk.Entry(frame, textvariable=scale_var,font=self.buttonfont)
+            def scalebox_command(var):
+                sf = float(scale_var.get())
+                modvel,modflux,modflux_e = datafile_2_array(self.outfile,isint=False,zipped=True)
+                modflux= [i * np.amax(self.obsflux)/np.amax(modflux) * sf for i in modflux]
+                
+                chi = chi_sq(self.obsflux,modflux,obs_err,modflux_e) 
+                chi = round(chi,2)
+                self.chi_text.delete(1.0,6.0)
+                self.chi_text.insert(tk.END,str(chi))
+                
+                self.ax.plot(modvel,modflux) 
+                self.figure_canvas.draw()
+            
+            scale_var_entry = tk.Entry(frame_5, textvariable=scale_var)
+            scale_var_entry.bind("<Return>", scalebox_command)   
             scale_var_entry.pack(fill = 'x', side=tk.LEFT, padx='20',pady='10')
+  
      
-     def scalebox_command(self,var):
-         print("hi")
-         #print(var.get())
-     
+
+
      def clear_pane(self,frame):
          self.fig.clear() #clear your figure
          self.initialise_plotwindow(frame)
@@ -401,6 +410,10 @@ for line in fi:
    #unless otherwise specified via the interactive button, the default dust distribition is smooth
    if  'fraction in clumps' in line:                
                  line=replace_str('0.0',0,line)
+                  
+   if  'dustData' in line:
+                 line=replace_str('\'dustData/sil-dlee.nk\'',1,line)        
+        
   
    sys.stdout.write(line)  
 
@@ -441,6 +454,9 @@ if __name__ == '__main__':
   Plotting_window.initialise_plotwindow(frame_3)
   Plotting_window.make_reset_button(frame_3)
   Plotting_window.make_model_scalebox(frame_5)
+      
+  
+         #print(var.get())
 
   frame_1.place(x=950,y=530)
   frame_2.place(x=980,y=0) 
@@ -450,258 +466,3 @@ if __name__ == '__main__':
   
 
   rootwindow.mainloop()
-
-
-'''
-
-########################################################################################################################################################
-##########setting environment parameters and creating widgets for the observed spectral line and damocles model comparison plotting window #############
-########################################################################################################################################################
-
-fig2 = plt.figure(2,figsize=(8,8))
-plt.xlabel("Velocity km/s",fontsize=20)
-plt.ylabel("Brightness",fontsize=20)
-plt.tick_params(axis='both', which='major',labelsize=20)
-
-#button to clear line emission plotting window
-resetax = plt.axes([0.25, 0.0, 0.65, 0.04])
-button = Button(resetax, 'Clear')
-
-#button to add in factor by which to rescale model line profile with
-scaleax = plt.axes([0.5, 0.1, 0.2, 0.04])
-scale_input = TextBox(scaleax, 'Scale model line profile by:', initial='1.0', color='.95', hovercolor='1', label_pad=0.01)
-
-plt.subplots_adjust(left=0.25, bottom=0.25)
-
-ax = fig2.add_subplot(111)
-
-trim_lims_vel = convert_wav_to_vel(trim_lims,lam_o,wavelength_peak_1*10.0)
-ax.set_xlim([trim_lims_vel[0],trim_lims_vel[1]])
-
-plt.plot(obsvels,obsflux)
-
-########################################################################################################################################################
-            ##########setting environment parameters and creating widgets for the 3D grid and sliders plotting window #############
-########################################################################################################################################################
-
-
-
-fig = plt.figure(1,figsize=(10,10))
-ax = fig.add_subplot(111, projection='3d')
-plt.subplots_adjust(left=0.25, bottom=0.25)
-
-
-x,y,z,d = make_Grid(v_max_init,Rrat_init,rho_index_init,age_d,grid_divs)
-grid_lim= np.amax(x)
-setax(ax,grid_lim)
-
-
-l = ax.scatter(x,y,z,c=d,cmap="nipy_spectral")
-cbar = fig.colorbar(l)
-cbar.set_label('density', rotation=270,size=18,labelpad=20)
-cbar.ax.tick_params(labelsize=13)
-
-
-
-ax_vmax = plt.axes([0.25, 0.0, 0.65, 0.02])
-ax_r = plt.axes([0.25, 0.05, 0.65, 0.02])
-ax_rho = plt.axes([0.25, 0.1, 0.65, 0.02])
-ax_dm = plt.axes([0.25, 0.15, 0.65, 0.02])
-ax_gs = plt.axes([0.25, 0.2, 0.65, 0.02])
-
-s_vmax = Slider(ax_vmax, 'V$_{max}$', 1000, 15000,valinit=v_max_init)
-s_vmax.label.set_size(15)
-s_r = Slider(ax_r, 'R$_{in}$/R$_{out}$', 0.001, 1,valinit=Rrat_init)
-s_r.label.set_size(15)
-s_rho = Slider(ax_rho, r'$\beta$', 0.001, 3,valinit=rho_index_init)
-s_rho.label.set_size(15)
-s_dm = Slider(ax_dm, 'Dust mass (M$\odot$)', 0.0, 0.005,valfmt='%9.7f',valinit=mdust_init)
-s_dm.label.set_size(15)
-s_gs = Slider(ax_gs, 'Grain radius ($\mu$m)', 0.005, 0.5,valinit=grain_size_init)
-s_gs.label.set_size(15)
-
-#IF AMC BOX IS TICKED THEN 100% AMC USED, IF NOT THEN 100 SIL IS USED
-amc_ax= plt.axes([0.15, 0.26, 0.08, 0.07])
-amc_button=CheckButtons(amc_ax, ['AmC?'],actives=[False])
-
-#IF CLUMP BOX IS TICKED THEN CLUMPED DISTRIBUTION IS USED, IF NOT THEN A SMOOTH ONE IS
-clump_ax= plt.axes([0.21, 0.26, 0.1, 0.07])
-clump_button=CheckButtons(clump_ax, ['Clumped?'],actives=[False])
-
-
-
-
-
-
-###EVERY TIME A SLIDER OR BUTTON IS INTERACTED WITH, THIS FUNCTION IS CALLED####
-
-def update(val):
-  #plt.cla()
-  
-  
-    
-  def reset(event):
-    plt.cla()
-    plt.tick_params(axis='both', which='major',labelsize=20)
-    
-    obs_file = path +"/" + obsfile
-    obswav,obsflux= datafile_2_array(obs_file,isint=False,zipped=True)
-  
-    obswav,obsflux = trim_wav_flux(obswav,obsflux,trim_lims[0],trim_lims[1])
-    obsflux = [i-4.2e-17 for i in obsflux]
-    obsflux = snip_spect(obswav,obsflux,*snip_regs)
-    lam_o= (1.0+z_red)*wavelength_peak_1*10.0
-    obsvels = convert_wav_to_vel(obswav,lam_o,wavelength_peak_1*10.0)
-
-    plt.plot(obsvels,obsflux)
-    plt.xlabel("Velocity km/s",fontsize=20)
-    plt.ylabel("Brightness",fontsize=20)
-
-  
-  def set_amc(event):
-      
-      fi3 = fileinput.FileInput(files=(spec_file),inplace=True)
-      if amc_button.get_status() == [True]: 
-          for line in fi3:	           
-            if  'dustData' in line:                 
-                 line=replace_str('\'dustData/amC-zb1.nk\'',1,line)
-            sys.stdout.write(line)     
-            
-      else:
-         for line in fi3:	
-             if  'dustData' in line:
-                 line=replace_str('\'dustData/sil-dlee.nk\'',1,line)        
-             sys.stdout.write(line)
-      fi3.close()
-  
-
-  def refit_scaled(event,arg1,arg2,arg3,arg4,arg5,arg6):
-               
-
-        arg1 = [i * float(scale_input.text) for i in arg1]
-
-        chi_sq_rescale = chi_sq(arg2,arg1,arg3,arg4)
-        print("chi sq: ",chi_sq_rescale)
-
-        plt.plot(arg5,arg1)
-        
-   #     frame3=plt.text(3500,np.amax(obsflux)-np.amax(obsflux)/10,"chi sq=" + str(chi_sq_rescale),fontsize=20)  
-        
-        arg6.canvas.draw()
-     #   Artist.remove(frame3)	
-  
-  def set_clump(event): 
-      fi2 = fileinput.FileInput(files=(dust_file),inplace=True)
-      if clump_button.get_status() == [True]:        
-          for line in fi2:	             
-            if  'fraction' in line:                
-                 line=replace_str('1.0',0,line)
-            sys.stdout.write(line)     
-            
-      else:
-         for line in fi2:	
-             if  'fraction' in line:
-                 line=replace_str('0.0',0,line)         
-             sys.stdout.write(line)
-      fi2.close()
-  
-  
-  #if any of the buttons are pressed, this calls the inset functions
-  amc_button.on_clicked(set_amc)
-  clump_button.on_clicked(set_clump)
-
-  button.on_clicked(reset)
- 
-  newv = s_vmax.val
-  newr = s_r.val
-  newrho = s_rho.val
-  newdm = s_dm.val
-  newgs	= s_gs.val
-  
-  
-  #remaking the 3D grit plot with new slider values
-  plt.figure(1)
-  ax.clear()
-   
-  x,y,z,d = make_Grid(newv,newr,newrho,age_d,grid_divs)
-  grid_lim= np.amax(x)
-  setax(ax,grid_lim)
-  
-  l= ax.scatter(x,y,z,c=d,cmap="nipy_spectral") # update the plot
-  
-  
-
-
-
-
-  #This line is calling damocles to run after all input parameter files have been updated 
-  model.run_damocles_wrap()    
-
-  #reading model output file after damocles has run
-  outfile = path + "/output/integrated_line_profile_binned.out"
-  vel,flux,flux_e= datafile_2_array(outfile,isint=False,zipped=True)
-  
-  #file that tells you tau and chi sq
-  mod_prop = path + "/output/model_properties.out"
-  props = datafile_2_array(mod_prop,isint=False,zipped=False)
-
-  #run chi sq here 
-  #scaling model flux by additional factor provided in Fig 2
-  
-  #have to use the lambda function to pass the refit_scaled def more than one arg, as by default it only accepts one, the event handler
-  
-
-  
-  
-  #extracting values from model_properties.out file by word matching. We then present the reduced chi sq value, as this is the most useful for determining whether a model is a good fit to the data
-  for i in props:      
-        if 'optical' in i and 'depth' in i and 'wavelength' in i and 'cell' not in i and '(absn)' not in i:
-           tau = i[-1]
-
-
-  
-  scale_input.on_submit(lambda event, args=(flux,obsflux,obs_err,flux_e,vel,fig2): refit_scaled(event,*args))
-  flux = [i * float(scale_input.text) for i in flux]
-  
-  
-  chi_sqq = chi_sq(obsflux,flux,obs_err,flux_e) 
-        
-  
-  
-  ###plotting model against observed line profile panel
-  
-  plt.figure(2)
-  
-  plt.plot(vel,flux)
-  
-      
-  plt.tick_params(axis='both', which='major',labelsize=20)
-	
-  plt.xlabel("Velocity km/s",fontsize=20)
-  plt.ylabel("Brightness",fontsize=20)
-  
-  frame1=plt.text(5000,np.amax(obsflux),"tau=" + str(tau),fontsize=20)
-  frame2=plt.text(3500,np.amax(obsflux)-np.amax(obsflux)/10,"chi sq=" + str(chi_sqq),fontsize=20)        
-  
-
-  
-  #need to reconfigure this so every time a button is activated the text is not removed
-  fig2.canvas.draw()
-  Artist.remove(frame1)
-  Artist.remove(frame2)	
-  
- 
-  
-  
-
-s_vmax.on_changed(update)
-s_r.on_changed(update)
-s_rho.on_changed(update)
-s_dm.on_changed(update)
-s_gs.on_changed(update)
-
-
-plt.show()
-'''
-
-#'''
